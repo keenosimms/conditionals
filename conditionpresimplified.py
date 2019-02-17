b1e5da78dacf69b43ae78a00b3a4df0f6da460bf
Keeno Simms <46448798+keenosimms@users.noreply.github.com>
2019-02-17 15:55:00 -0500

    Add files via upload

---
+++ conditionpresimplified.py
@@ -0,0 +1,10 @@
+oranges = 5
+grapes = 7
+
+if oranges == grapes:
+   print('oranges and grapes are equal')
+else:
+    if oranges < grapes:
+       print('There are less oranges than grapes')
+    else:
+       print('There are more oranges than grapes')
