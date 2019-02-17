a5630b65a5774cfe0896ca727ff81a102d6022c2
Keeno Simms <46448798+keenosimms@users.noreply.github.com>
2019-02-17 14:38:22 -0500

    Add files via upload

---
+++ conditionals.py
@@ -0,0 +1,15 @@
+#Simple Chained Conditional Example
+#Date February 17th, 2019
+#by Keeno Simms
+
+red_apples = 33
+green_apples = 72
+yellow_apples = 100
+
+if red_apples > yellow_apples:
+    print('There are less red apples than green apples')
+elif yellow_apples < green_apples:
+    print ('There are more yellow apples than green apples')
+else: 
+    print('We need more apples')
+
---
+++ nestedcondition.py
@@ -0,0 +1,13 @@
+#Nested conditional 
+#February 17th, 2019
+#by Keeno Simms
+
+if 587.00 == 274.76:
+   print('587.00 and 274.76 are equal')
+else:
+    if 587.00 < 274.76:
+        print('587.00 is less than 274.76')
+    else:
+        print('587.00 is more than 274.76')
+
+    
